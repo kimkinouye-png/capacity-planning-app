86012f270cc74d0c7a447e1c2bc311e738c83e8c
Kimberly K. Inouye <kki@mac.lan>
2026-01-10 19:20:32 -0600

    Scaffold Vite + React + Chakra app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Box, Heading } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <Box p={8}>
+      <Heading size="lg">Design Capacity Planning Tool</Heading>
+    </Box>
+  )
+}
+
+export default App
